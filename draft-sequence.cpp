7db8f1d6cc1b66a6722d145a5f17a5ff9e23a7d5
Lenoir Christopher <lenoirc.fr@gmail.com>
2019-02-03 19:43:50 +0100

    Added intro of Justice, Waters of Nazareth on button 1 and 2. 3 Patterns with "Sequence" class were made for this

--- draft-sequence.cpp
+++ draft-sequence.cpp
@@ -28,12 +28,12 @@ class Sequence
         int * pas;
 
         void jouer() const;
-        void composer();
+        void composer(int * composition);
 };
 Sequence::Sequence(int tempo, int resolution)
 {
     bpm = tempo;
-    ronde = tempo/60*1000;
+    ronde = (tempo/60)*1000;
     resolution_mesure = resolution;
     // pas = int pas[resolution];
 }
@@ -48,23 +48,39 @@ void Sequence::jouer() const
         }
     }
 }
-void Sequence::composer()
+void Sequence::composer(int * composition)
 {
-    // pas = new int[]{};
+    // pas = new int[resolution_mesure]composition;
+    // pas = new int[resolution_mesure];
+    
+    // for(int i = 0; i < composition.size(); i++)
+    // {
+    //     pas[i] = composition[i];
+    // }
 }
 
 // ------------------------------------------------ //
 // ----- Utilisation de la classe en contexte ----- //
 // ------------------------------------------------ //
 
-Sequence sequence1(128, 8);
-Sequence sequence2(128, 16);
+int tempo = 128;
+
+Sequence sequence1(tempo, 4);
+Sequence sequence2(tempo, 8);
+Sequence sequence3(tempo, 16);
 
 void setup()
 {
     // Ici on compose
-    sequence1.pas = new int[sequence1.resolution_mesure]{110,0,220,0,110,0,220,0};
-    sequence2.pas = new int[sequence2.resolution_mesure]{110,110,220,0,110,0,220,0,110,220,220,0,110,0,220,0};
+    // sequence1.composer(new int[] = {110,0,220,0,110,0,220,0});
+    sequence1.pas = new int[sequence1.resolution_mesure]{196,174.61,196,196};
+    sequence2.pas = new int[sequence2.resolution_mesure]{196,196,196,0,
+                                                        196,196,196,0};
+    sequence3.pas = new int[sequence3.resolution_mesure]{293.66,293.66,293.66,293.66,
+                                                        293.66,349.23,0,293.66,
+                                                        0,293.66,587.33,0,
+                                                        0,0,698.46,698.46
+    };
 
     // double * arr = new double[3]{1,2,3};
     led1.allume();
@@ -77,9 +93,15 @@ void loop()
     if (button1.aEteAppuye()) {
         // Ici on joue notre séquence une fois
         sequence1.jouer();
+        sequence2.jouer();
+        sequence2.jouer();
+        sequence2.jouer();
     }
     if (button2.aEteAppuye()) {
         // Ici on joue notre séquence une fois
-        sequence2.jouer();
+        sequence3.jouer();
+        sequence3.jouer();
+        sequence3.jouer();
+        sequence3.jouer();
     }
 }
